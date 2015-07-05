1aa07bdbfa2e0820c5af8949b28b64f81892f269
ylm <2540239530@qq.com>
2015-07-05 11:11:19 +0800

    commit info

---
+++ js/memory.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	// $(".photoWall ul li").each(function(){
+	// 	$(this).mouseover(function(){
+	// 		$(this).find("figcaption").css("z-index","2");
+	// 	});	
+	// 	$(this).mouseleave(function(){
+	// 		$(this).find("figcaption").css("z-index","-2");
+	// 	});
+	// });
+});
